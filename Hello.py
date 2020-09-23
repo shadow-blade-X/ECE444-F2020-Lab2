843f001c18c67f81d84b10e3078bc6b9c91b7fbc
shadow-blade-X <jingxuansu1006@gmail.com>
2020-09-23 11:02:40 -0400

    Update For activity 2
    Reproduce example 2-2
    It now can show content based on <user name> in http://127.0.0.1:5000/user/<user name>

--- Hello.py
+++ Hello.py
@@ -3,9 +3,12 @@ from flask import Flask
 appJX = Flask(__name__)
 
 @appJX.route('/')
-
 def index():
  return '<h1>Hello World!</h1>'
+
+@appJX.route('/user/<name>')
+def user(name):
+ return '<h1>Hello, %s!</h1>' % name
 
 if __name__ == '__main__':
  appJX.run(debug=True)
